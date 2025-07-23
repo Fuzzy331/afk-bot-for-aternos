13594cef4100149ff03957e71588292fda12fc81
Fuzzy331 <tershalil3312@gmail.com>
2025-07-24 01:57:10 +0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
+var host = data["Herotif.aternos.me"];
 var username = data["name"]
 var bot = mineflayer.createBot({
   host: host,
